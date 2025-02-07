c272bc6542e15dd31ab1ac73651ee41102abb0bf
sebastian-sultz <hiadarshtripathi@gmail.com>
2025-02-08 04:03:11 +0530

    dist

--- vite.config.js
+++ vite.config.js
@@ -4,24 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('react')) {
-              return id.includes('react-dom') 
-                ? 'vendor-react-dom' 
-                : 'vendor-react';
-            }
-            if (id.includes('framer-motion')) return 'vendor-framer-motion';
-            if (id.includes('lodash')) return 'vendor-lodash';
-            return 'vendor-other';
-          }
-        }
-      }
-    },
-    chunkSizeWarningLimit: 1000 // Optional: Increase if needed
-  }
 })
-
